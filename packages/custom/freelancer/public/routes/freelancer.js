3626c306b4e58094681cef83bd1cfe3dee2e70be
unknown <gauravscorp05@gmail.com>
2015-12-12 03:56:58 +0530

    freelancer coworker ui and payment redirects

--- packages/custom/freelancer/public/routes/freelancer.js
+++ packages/custom/freelancer/public/routes/freelancer.js
@@ -14,7 +14,15 @@ angular.module('mean.freelancer').config(['$stateProvider',
             .state('freelancer_landing_page', {
                 url: '/freelancer/storefront/:freelancerId',
                 templateUrl: 'freelancer/views/freelancer_landing.html'
-            });
+            })
+            .state('payment_success', {
+                url: '/payment_status_success/:order_id',
+                templateUrl: 'freelancer/views/payment_success.html'
+            })
+            .state('payment_failed', {
+                url: '/payment_status_failed/:order_id',
+                templateUrl: 'freelancer/views/payment_failed.html'
+            })
 
     }
 ]);
